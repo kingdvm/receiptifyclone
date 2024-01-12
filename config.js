fca5f6c6a8f0c71e5b4aeaac78ee5fab22b09759
kingdvm <kingdvm.xo@gmail.com>
2024-01-12 13:10:22 -0700

    Updated basic config information and values

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * https://beta.developer.spotify.com/dashboard
  */
 
-const client_id = 'YOUR_CLIENT_ID';
-const redirect_uri = 'YOUR_CALLBACK_URL';
-const client_secret = 'YOUR_CLIENT_SECRET';
+const client_id = 'b6eaeaf0f1684172bb010164ccd2c1a0';
+const redirect_uri = 'http://localhost:3000';
+const client_secret = 'c5d28d0700e74ed994378ecea0f5afba';
